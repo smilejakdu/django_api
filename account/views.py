a64a687e143190adb156993f7bb1c71e17a3244a
smilejakdu <ash982416@gmail.com>
2020-02-07 11:18:33 +0900

    Account_modify

--- account/views.py
+++ account/views.py
@@ -9,9 +9,8 @@ class AccountView(View):  # 회원가입
 
     def post(self, request):
         data = json.loads(request.body)
-        account_data = Account.objects.values()
 
-        if data['email'] in list(account_data):
+        if Account.objects.filter(email=data['email']).exists():
             return JsonResponse({'email': '존재하는 이메일 입니다.'}, status=200)
 
         if data['password'] is None or data['password'] == '':
@@ -33,11 +32,14 @@ class LoginView(View):  # 로그인
     def post(self, request):
         data = json.loads(request.body)
 
-        if Account.objects.filter(email=data['email']) and data['password'] == Account.objects.get(
-                email=data['email']).password:
-            return JsonResponse({'email': "{} 님 안녕하세요.".format(data['email'])})
+        if Account.objects.filter(email=data['email']).exists():
+            # 만약에 email 이 존재한다면 else == > error
+            if data['password'] == Account.objects.get(email=data['email']).password:
+                return JsonResponse({'email': "{} 님 안녕하세요.".format(data['email'])}, status=200)
+            else:
+                return JsonResponse({"error": "error"})
         else:
-            return JsonResponse({"error": "email 이나 password 틀림"})
+            return JsonResponse({"error": "error"})
 
     def get(self, request):  # 확인하기 위해서
         return JsonResponse({'로그인페이지': ''}, status=200)
